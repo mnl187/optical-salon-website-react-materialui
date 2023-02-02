969c4a306ca8c98f59c991505d5406188d2e5b12
mnl187 <mnl187@gmail.com>
2023-02-02 19:48:22 +0100

    dodanie podstrony Services

---
+++ src/Services/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+export const Services = () => {
+    return (
+        <Container>
+            <Typography variant="h4">Usługi</Typography>
+            <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet
+                tellus at ex sagittis, id malesuada metus placerat. Sed malesuada
+                sollicitudin facilisis.
+            </Typography>
+        </Container>
+    );
+};
