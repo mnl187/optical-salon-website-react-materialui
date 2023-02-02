27f8db13e6d5822b502733c7cc49965012152f3c
mnl187 <mnl187@gmail.com>
2023-02-02 19:07:54 +0100

    dodanie komponentu panelu nawigacyjnego Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Icon from '@mui/material/Icon';
+import {Box, Button} from "@mui/material";
+import {BasicTabs} from "./TabPanel/TabPanel";
 
-function App() {
+
+export function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BasicTabs/>
+
     </div>
   );
 }
-
-export default App;
